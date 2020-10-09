874a9593056396cfafb9b7340955eb7a7065b5da
Avatar Azka <avatar.azka@gmail.com>
2020-10-10 00:06:31 +0700

    Update via bash alias

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -4,7 +4,7 @@
 # Tue 29 Sep 2020 11:02:39 AM WIB
 # PALSU BANGET SEKALI UBAH
 
-FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
 rm -f $SHA $SHA.asc
@@ -22,3 +22,5 @@ echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 exit 0
+
+# Mon Sep 28 21:05:04 WIB 2020
