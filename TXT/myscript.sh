5990d0eb576556a10efc8be7fa9cf75dd4d8eaa8
siepenmaru <avatar.azka@gmail.com>
2020-10-10 17:21:01 +0700

    Update via bash alias

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Cicak bin Kadal
-# Mon Sep 28 22:03:27 WIB 2020
-# Tue 29 Sep 2020 11:02:39 AM WIB
-# PALSU BANGET SEKALI UBAH
+# Avatar Azka / siepenmaru / 1906426802
+# Last Edit: 10 Oct 2020
 
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
@@ -22,5 +20,3 @@ echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 exit 0
-
-# Mon Sep 28 21:05:04 WIB 2020
