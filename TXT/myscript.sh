6bf32e650f6ac3d9bd5f8415ca2b6b9e71825e9b
Avatar Azka <avatar.azka@gmail.com>
2020-11-19 00:33:31 +0700

    Update via bash alias

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Avatar Azka / siepenmaru / 1906426802
-# Last Edit: 10 Oct 2020
+# Cicak bin Kadal
+# Tue 13 Oct 2020 10:37:14 AM WIB
 
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
+echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
 
 echo "sha256sum $FILES > $SHA"
@@ -20,3 +21,6 @@ echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 exit 0
+
+# Mon Sep 28 21:05:04 WIB 2020
+# Tue 29 Sep 2020 11:02:39 AM WIB
